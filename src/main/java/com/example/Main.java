2ba8aca3293b0dda9b694850f6e4b7f583847a47
Vitamin92 <elrimbor@gmail.com>
2016-07-13 21:37:21 +0300

    first commit

---
+++ src/main/java/com/example/Main.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class Main {
+
+	public static void main(String[] args) {
+	print("Boul shit");
+	}
+public static void print(String s){
+	System.out.println(s);
+}
+}
